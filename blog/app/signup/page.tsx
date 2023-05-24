85d5c2c96995d966262f5597f9197c929e121e44
Dohyun kim <dh@Dohyuns-MacBook-Air.local>
2023-05-24 14:50:58 -0400

    modify confidentials and setup signup form/page

---
+++ blog/app/signup/page.tsx
@@ -0,0 +1,13 @@
+// pages/CreatePostPage.tsx
+import SignupForm from '../../components/SignupForm';
+
+const page: React.FC = () => {
+  return (
+    <div>
+      <h1>Signup page</h1>
+      <SignupForm />
+    </div>
+  );
+};
+
+export default page;
---
+++ blog/components/SignupForm.tsx
@@ -0,0 +1,39 @@
+"use client"; // This is a client component
+import { useState } from 'react';
+import firebase from '../utils/firebase';
+
+const SignupForm: React.FC = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+
+  console.log(process.env.NEXT_PUBLIC_API_KEY)
+
+  const handleSignup = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    try {
+      const { user } = await firebase.auth().createUserWithEmailAndPassword(email, password);
+      console.log('User created:', user);
+      // Handle successful signup
+    } catch (error) {
+      console.error('Error signing up:', error);
+      // Handle signup error
+    }
+  };
+
+  return (
+    <form onSubmit={handleSignup}>
+      <input type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
+      <input
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button type="submit">Sign Up</button>
+    </form>
+  );
+};
+
+export default SignupForm;
